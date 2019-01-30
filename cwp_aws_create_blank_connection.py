e06a3fde8970fdbca493b5a669424c1dd0cf5b99
Constancio <33767222+ConstiF@users.noreply.github.com>
2019-01-29 16:31:34 -0800

    Update cwp_aws_create_blank_connection.py

--- cwp_aws_create_blank_connection.py
+++ cwp_aws_create_blank_connection.py
@@ -43,7 +43,7 @@ def createconnection():
   #Extracting auth token
   accesstoken= token['access_token']
   accesstoken = "Bearer " + accesstoken
-  print("Access Token is : "+str(accesstoken))
+  #print("Access Token is : "+str(accesstoken))
   createurl = serverURL+"/dcs-service/dcscloud/v1/cpif/cloud_connections"
   createheader = {"Authorization": accesstoken ,"Content-type": "application/json" ,'x-epmp-customer-id' : customerID , 'x-epmp-domain-id' : domainID}
   header1 = {"Content-type": "application/json","Authorization": accesstoken ,'x-epmp-customer-id' : customerID , 'x-epmp-domain-id' : domainID}
